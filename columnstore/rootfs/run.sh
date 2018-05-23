753c6296593615311a7f361ea5d3c66cd809a46f
Edwin Hoksberg <mail@edwinhoksberg.nl>
2018-05-23 15:38:25 +0200

    Refactor docker run script

--- columnstore/rootfs/run.sh
+++ columnstore/rootfs/run.sh
@@ -1,19 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 mysql="/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -e"
 flag=/initialized
 
-trap "/usr/local/mariadb/columnstore/bin/mcsadmin shutdownSystem y" INT TERM
+function columnstore_shutdown {
+    /usr/local/mariadb/columnstore/bin/mcsadmin shutdownSystem y
+}
 
-echo "Starting columnstore..."
-/usr/local/mariadb/columnstore/bin/mcsadmin startSystem
+function columnstore_startup {
+    # Starting process manager
+    /usr/local/mariadb/columnstore/bin/ProcMon &
 
+    # Start columnstore
+    /usr/local/mariadb/columnstore/bin/mcsadmin startSystem
+}
+
+echo "[Docker] Starting columnstore..."
+columnstore_startup
+
+# Initial database setup
 if [ ! -f $flag ]; then
     # Generate a random password for the root user
     if [ "$MYSQL_RANDOM_ROOT_PASSWORD" = "yes" ]; then
         MYSQL_ROOT_PASSWORD=$(pwgen 20 1)
 
-        echo "Generated password for root user: ${MYSQL_ROOT_PASSWORD}"
+        echo "[Docker] Generated password for root user: ${MYSQL_ROOT_PASSWORD}"
     fi
 
     # Set the root password, if one was provided
@@ -24,7 +35,12 @@ if [ ! -f $flag ]; then
     touch $flag
 fi
 
-echo "Started columnstore, waiting for SIGINT/SIGTERM..."
+# Trap signals for detecting shutdown
+trap columnstore_shutdown INT TERM
+
+echo "[Docker] Started columnstore, waiting for SIGINT/SIGTERM..."
+
+# Start a infinitly waiting process and wait for it to shutdown
 sleep infinity &
 pid=$!
 
