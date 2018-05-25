b9cfe9a02f8901a7d39406bed52d0ac506e5c914
Edwin Hoksberg <mail@edwinhoksberg.nl>
2018-05-25 11:00:38 +0200

    Introduce versioning to docker images

--- build-all.sh
+++ build-all.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
-set -e
+set -ex
 
-current_dir=$(pwd)
+start_dir=$(pwd)
 namespace=edwinhoksberg
 
 # loop through each directory in current dir
 for d in */; do
     cd "$d"
+    current_dir=$(pwd)
 
-    # Get basename of directory and build docker image
-    name=$(basename "$d")
-    docker build -t "$namespace"/"$name" .
+    # loop through each version of the image
+    for dd in */; do
+        cd "$dd"
+
+        # Get basename of directory and build docker image
+        name=$(basename "$d")
+        tag=$(basename "$dd")
+        docker build -t "$namespace"/"$name":"$tag" .
+
+        cd "$current_dir"
+    done
 
     # return to the base directory
-    cd "$current_dir"
+    cd "$start_dir"
 done
